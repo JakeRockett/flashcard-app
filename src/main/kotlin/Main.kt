bf6adadb8fd6ccec89572829d4e4d4ebc430ba63
keystonedigital <dylanandjakebusiness@gmail.com>
2025-11-20 20:54:32 +0000

    Update #3: Update Flashcard App menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,62 @@
 package ie.setu
 
-fun mainMenu() : Int {
-    println("")
-    println("--------------------")
-    println("FLASHCARD STUDY APP")
-    println("--------------------")
-    println("FLASHCARD MENU")
-    println("   1) ")
-    println("   2) ")
-    println("   3) ")
-    println("   4) ")
-    println("   5) ")
-    println("--------------------")
-    println("   0) Exit ")
-    println("--------------------")
-    print("==>> ")
+import java.lang.System.exit
+
+fun main() {
+    runMenu()
+}
+
+fun flashCardMenu(): Int {
+    print("""
+          ----------------------------------
+          |        FLASHCARD APP           |
+          ----------------------------------
+          | FLASHCARD MENU                 |
+          |   1) Add a Flashcard           |
+          |   2) View All Flashcards       |
+          |   3) Study a Flashcard         |
+          |   4) Edit a Flashcard          |
+          ----------------------------------
+          |   0) Exit                      |
+          ----------------------------------
+          ==>> """)
     return readlnOrNull()?.toIntOrNull() ?: -1
+}
+
+
+
+fun runMenu() {
+    do {
+        val num = flashCardMenu()
+        when (num) {
+            1 -> addFlashCard()
+            2 -> viewFlashCard()
+            3 -> studyFlashCard()
+            4 -> editFlashCard()
+            0 -> exitApp()
+            else -> println("The number you chose was invalid. Try again: ")
+        }
+
+    } while (true)
+}
+
+fun addFlashCard() {
+
+}
+
+fun viewFlashCard() {
+
+}
+
+fun studyFlashCard() {
+
+}
+
+fun editFlashCard() {
+
+}
+
+fun exitApp() {
+    println("Goodbye for now.")
+    exit(0)
 }
